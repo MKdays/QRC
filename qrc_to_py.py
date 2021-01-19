d3752a9e3b2f356b2e387796ffaae27993922956
eliase1986@gmail.com <eliase1986@gmail.com>
2021-01-19 21:48:40 +0900

    update

--- qrc_to_py.py
+++ qrc_to_py.py
@@ -11,49 +11,30 @@ from glob import glob
 
 # 옵션 ------------------------------------------------------
 path_search = app_path
-path_out_1 = str(Path(app_path).parent) + "/ExcelKits/qt_resource"
-path_out_2 = str(Path(app_path).parent) + "/PyKooler/qt_resource"
-path_out_3 = str(Path(app_path).parent) + "/RuleMaker/qt_resource"
-
-# print(path_out)
-# as_is = ""
-# to_be = ""
+path_out = [
+    str(Path(app_path).parent) +"/ExcelKits/qt_resource",
+    str(Path(app_path).parent) + "/PyKooler/qt_resource",
+    str(Path(app_path).parent) + "/RuleMaker/qt_resource"
+]
 # -----------------------------------------------------------
 
-# work_list =[]
-# for i in os.listdir(path_search):
-#     if i.endswith(".ui") or i.endswith(".qrc"):
-#         work_list.append(i)
+work_list = glob("*.qrc") + glob("*.ui")
 
-work_list = glob("*.qrc")
+def ui_convert(raw):
+    for i in path_out:
+        os.system(f"pyside2-uic \"{raw}\" -o \"{i}/{Path(raw).stem}.py\"")
+        print(f"\"{i}/{Path(raw).stem}.py\"")
 
-# def ui_convert(i):
-#   os.system("pyside2-uic " + i + " -o " + path_out + "/" + Path(i).stem + ".py")
+def qrc_convert(raw):
+    for i in path_out:
+        os.system(f"pyside2-rcc \"{raw}\" -o \"{i}/{Path(raw).stem}.py\"")
+        print(f"\"{i}/{Path(raw).stem}.py\"")
 
-def qrc_convert(i):
-  print(path_out_1)
-  os.system("pyside2-rcc " + i + " -o " + path_out_1 + "/" + Path(i).stem + ".py")
-  print(path_out_2)
-  os.system("pyside2-rcc " + i + " -o " + path_out_2 + "/" + Path(i).stem + ".py")
-  print(path_out_3)
-  os.system("pyside2-rcc " + i + " -o " + path_out_3 + "/" + Path(i).stem + ".py")
-
-# def replace_text(i):
-#   with open(path_out + "/" + Path(i).stem + ".py", 'r', encoding="utf-8") as file :
-#     filedata = file.read()
-#   filedata = filedata.replace(as_is, to_be)
-#   with open(path_out + "/" + Path(i).stem + ".py", 'w', encoding="utf-8") as file:
-#     file.write(filedata)
-
-for i in work_list:
-  # print(i + " 완료")
-  # if i.endswith(".ui"):ui_convert(i)
-  # elif i.endswith(".qrc"):qrc_convert(i)
-  qrc_convert(i)
-#   replace_text(i)
+for raw in work_list:
+    if raw.endswith(".ui"):ui_convert(raw)
+    elif raw.endswith(".qrc"):qrc_convert(raw)
 
 try:shutil.rmtree("__pycache__") #폴더삭제
 except:pass
 
-print("OK")
 sleep(0.5)
